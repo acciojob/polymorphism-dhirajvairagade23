669e1427c805149b3c78ad31354060ca211a9cc3
Dhiraj <dhirajvairagade23@gmail.com>
2025-04-28 08:54:56 +0530

    Poly 4th commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,13 @@ package com.driver;
 public class Main {
     public static class Product{
 
-        public int Product(int x,int y){
+        public int product(int x,int y){
             return x*y;
         }
-        public int Product(int x,int y, int z){
+        public int product(int x,int y, int z){
             return x*y*z;
         }
-        public double Product(double x,double y){
+        public double product(double x,double y){
             return x*y;
         }
     }
@@ -18,9 +18,9 @@ public class Main {
     public static void main(String args[]){
 
         Product p=new Product();
-        int ans1 = p.Product(3,4);
-        int ans2 = p.Product(4,6,2);
-        double ans3 = p.Product(2,3);
+        int ans1 = p.product(3,4);
+        int ans2 = p.product(4,6,2);
+        double ans3 = p.product(2,3);
 
         System.out.println(ans1+" "+ans2+" "+ans3);
 
