bf90ddb4c6506062a6f78d9ae4ee1793ab5934c7
Dhiraj <dhirajvairagade23@gmail.com>
2025-04-28 08:58:44 +0530

    Poly 5th commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,7 +15,7 @@ public class Main {
     }
 
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         Product p=new Product();
         int ans1 = p.product(3,4);
